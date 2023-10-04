80c250be4f0947eb802aa9a8ff11a9b45cddb63d
Zuruuh <ziadi.mail.pro@gmail.com>
2023-10-04 22:16:48 +0200

    Works better

--- Justfile
+++ Justfile
@@ -1,12 +1,11 @@
 set dotenv-load := true
 
 build:
+    cargo build --release
     docker build --tag zuruh/discord-feur-bot-rs .
 
 run:
     docker run --env-file .env zuruh/discord-feur-bot-rs
-
-dev: build run
 
 build-prod:
     docker build --tag zuruh/discord-feur-bot-rs --push --platform linux/amd64 .
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, env};
+
 use dotenv::dotenv;
 use lazy_static::lazy_static;
 use rand::seq::SliceRandom;
@@ -8,12 +10,26 @@ use serenity::{
     prelude::{Context, EventHandler, GatewayIntents},
     Client,
 };
-use std::{collections::HashMap, env};
 
-const REPLIES_JSON: &str = std::include_str!("../replies.json");
 lazy_static! {
     #[derive(Debug)]
-    static ref REPLIES: HashMap<String, Vec<String>> = serde_json::from_str(REPLIES_JSON).unwrap();
+    static ref WHITESPACE_REGEX: Regex = Regex::new("\\W").unwrap();
+    static ref REPLIES: HashMap<String, (Regex, Vec<String>)> = {
+        let replies_json = std::include_str!("../replies.json");
+        let raw_replies: HashMap<String, Vec<String>> = serde_json::from_str(replies_json).unwrap();
+        let mut replies_map = HashMap::<String, (Regex, Vec<String>)>::new();
+
+        for (word, replies) in raw_replies.iter() {
+            replies_map.insert(
+                word.to_owned(),
+                (Regex::new(&format!("{word}$")).unwrap(), replies.to_owned())
+            );
+        }
+
+        drop(raw_replies);
+
+        replies_map
+    };
 }
 
 struct Handler;
@@ -29,18 +45,13 @@ impl EventHandler for Handler {
             "Received message from {} with content \"{}\"",
             &message.author.name, &message.content
         );
-        let content = Regex::new("\\W")
-            .unwrap()
+        let content = WHITESPACE_REGEX
             .replace_all(&message.content, "")
             .to_lowercase();
         println!("Parsed message to raw string \"{}\"", &content);
 
-        for keyword in REPLIES.keys() {
-            if Regex::new(&format!("{}$", &keyword))
-                .unwrap()
-                .is_match(&content)
-            {
-                let replies = REPLIES.get(keyword).unwrap();
+        for (_, (regex, replies)) in REPLIES.iter() {
+            if regex.is_match(&content) {
                 let reply = replies.choose(&mut rand::thread_rng()).unwrap();
 
                 message
